371ab73a6463df678985cc6b853e8f48f40f8b4b
lansanakeita <lansanakeita1900@gmail.com>
2025-06-17 15:56:25 +0200

    modification du backend pour corriger lerreur 500

--- mpci-api/src/Controller/Admin/DashboardController.php
+++ mpci-api/src/Controller/Admin/DashboardController.php
@@ -12,34 +12,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-
-
-
 
 #[AdminDashboard(routePath: '/mpci-administrateur', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
-    private $adminUrlGenerator;
 
-
-    public function __construct(AdminUrlGenerator $adminUrlGenerator)
-    {
-        $this->adminUrlGenerator = $adminUrlGenerator;
-    }
 
     public function index(): Response
     {
-        return $this->redirect($this->adminUrlGenerator->setController(EvenementCrudController::class)->generateUrl());
+        return parent::index();
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-        ->setTitle('<img src="/assets/images/logo.jpg" alt="Logo" style="height:70px;">');
+            ->setTitle('<img src="/assets/images/logo.jpg" alt="Logo" style="height:70px;">');
 
-            //->setTitle('Guinea Api');
     }
 
     public function configureMenuItems(): iterable
@@ -49,11 +37,5 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Participants', 'fas fa-user', Participant::class);
         yield MenuItem::linkToCrud('Panels', 'fas fa-list', Panel::class);
         yield MenuItem::linkToCrud('utilisateurs', 'fas fa-user', Utilisateur::class);
-    }
-
-    public function configureAssets(): Assets
-    {
-        return Assets::new()
-            ->addCssFile('assets/styles/app.css');
     }
 }
